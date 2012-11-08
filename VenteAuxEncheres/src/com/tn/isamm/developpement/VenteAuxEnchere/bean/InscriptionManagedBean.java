6d5471eaf576f07688acdd6a008c5833f9161f1f
Mohamed Salah El Béji <mohamed.salah.elbeji@gmail.com>
2012-11-08 21:43:48 +0100

    Version 0.00000001

--- VenteAuxEncheres/src/com/tn/isamm/developpement/VenteAuxEnchere/bean/InscriptionManagedBean.java
+++ VenteAuxEncheres/src/com/tn/isamm/developpement/VenteAuxEnchere/bean/InscriptionManagedBean.java
@@ -2,11 +2,16 @@ package com.tn.isamm.developpement.VenteAuxEnchere.bean;
 
 import java.io.Serializable;
 import java.util.List;
+
 import javax.annotation.PostConstruct;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+
+import com.tn.isamm.developpement.VenteAuxEnchere.dao.EnchereurDao;
 import com.tn.isamm.developpement.VenteAuxEnchere.dao.VendeurDao;
+import com.tn.isamm.developpement.VenteAuxEnchere.daoImp.EnchereurDaoImp;
 import com.tn.isamm.developpement.VenteAuxEnchere.daoImp.VendeurDaoImp;
+import com.tn.isamm.developpement.VenteAuxEnchere.model.Enchereur;
 import com.tn.isamm.developpement.VenteAuxEnchere.model.Vendeur;
 
 @ManagedBean(name = "inscriptionMB")
@@ -18,15 +23,18 @@ public class InscriptionManagedBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 1L;
 	private Vendeur vendeur = new Vendeur();
+	private Enchereur enchereur = new Enchereur();
+	private EnchereurDao enchereurDao = new EnchereurDaoImp();
 	private VendeurDao vendeurDao = new VendeurDaoImp();
 	private List<Vendeur> vendeurList;
+	private List<Enchereur> enchereurList;
 
 	@PostConstruct
 	public void initBean() {
 
 		try {
 			vendeurList = vendeurDao.getAll();
-
+			enchereurList = enchereurDao.getAll();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -38,8 +46,23 @@ public class InscriptionManagedBean implements Serializable {
 
 			vendeurDao.ajouterVendeur(vendeur);
 			vendeurList = vendeurDao.getAll();
-
 			vendeur = new Vendeur();
+
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+
+		}
+
+	}
+
+	public void addEnchereur() {
+		try {
+
+			enchereurDao.ajouterEnchereur(enchereur);
+			enchereurList = enchereurDao.getAll();
+			enchereur = new Enchereur();
+
 		} catch (Exception e) {
 			// TODO: handle exception
 			e.printStackTrace();
@@ -65,6 +88,24 @@ public class InscriptionManagedBean implements Serializable {
 
 	public void setVendeurList(List<Vendeur> vendeurList) {
 		this.vendeurList = vendeurList;
+	}
+
+	public Enchereur getEnchereur() {
+		return enchereur;
+	}
+
+	public void setEnchereur(Enchereur enchereur) {
+		this.enchereur = enchereur;
+	}
+
+	public List<Enchereur> getEnchereurList() {
+		if (enchereurList == null)
+			enchereurList = enchereurDao.getAll();
+		return enchereurList;
+	}
+
+	public void setEnchereurList(List<Enchereur> enchereurList) {
+		this.enchereurList = enchereurList;
 	}
 
 }
